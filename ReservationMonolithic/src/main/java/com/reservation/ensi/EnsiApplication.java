2f2ada1358ad08bc79635cb8bf87de392b618200
skander6 <khdhiriskander@gmail.com>
2020-01-11 13:38:26 +0100

    first push

---
+++ ReservationMonolithic/src/main/java/com/reservation/ensi/EnsiApplication.java
@@ -0,0 +1,13 @@
+package com.reservation.ensi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnsiApplication.class, args);
+    }
+
+}
---
+++ ReservationMonolithic/src/test/java/com/reservation/ensi/EnsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.ensi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
