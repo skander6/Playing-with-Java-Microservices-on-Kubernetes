298c7a0992b31aa7ef72aae93c667995c1b9e124
Ghaith Jemai <ghaith.jemai@ensi-uma.tn>
2020-01-11 16:25:37 +0100

    volupdate

--- ReservationMonolithic/src/main/java/com/reservation/ensi/contoller/VolController.java
+++ ReservationMonolithic/src/main/java/com/reservation/ensi/contoller/VolController.java
@@ -1,15 +1,23 @@
 package com.reservation.ensi.contoller;
 
 
+import com.reservation.ensi.DTO.VolDTO;
+import com.reservation.ensi.model.Vol;
 import com.reservation.ensi.service.VolService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/vols")
+@RequestMapping("/voles")
 public class VolController {
     @Autowired
     private VolService volService;
+    @PostMapping
+    public Vol create(@RequestBody VolDTO volDTO){
+        return this.volService.create(volDTO);
+    }
 
 }
