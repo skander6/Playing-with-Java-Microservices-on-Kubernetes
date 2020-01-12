f1884b25c2114027ea0d9d4c4802353750e95f44
MedAmineRekik <medamine.rekik20@gmail.com>
2020-01-12 15:57:09 +0100

    done microservices

---
+++ ReservationMicroservices/logistic/src/main/java/com/ensi/logistic/model/Vol.java
@@ -0,0 +1,48 @@
+package com.reservation.ensi.model;
+
+
+import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+public class Vol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String depart;
+    private String arrive;
+    private Date dateDepart;
+    private Date dateArrive;
+
+    private  Long nbrePlaceDispo;
+    @ManyToOne
+    private Plane plane;
+
+    public Vol(){
+
+    }
+    public Vol(String depart,String arrive,Date dateDepart,Date dateArrive,Long nbrePlaceDispo,Plane plane){
+        this.depart=depart;
+        this.arrive=arrive;
+        this.dateArrive=dateArrive;
+        this.nbrePlaceDispo=nbrePlaceDispo;
+       this.plane=plane;
+    }
+
+    public Long getNbrePlaceDispo() {
+        return nbrePlaceDispo;
+    }
+
+    public void setNbrePlaceDispo(Long nbrePlaceDispo) {
+        this.nbrePlaceDispo = nbrePlaceDispo;
+    }
+
+    public void setPlane(Plane plane) {
+        this.plane = plane;
+    }
+}
+
---
+++ ReservationMicroservices/logistic/src/main/java/com/ensi/logistic/service/PlaneService.java
@@ -0,0 +1,38 @@
+package com.reservation.ensi.service;
+
+
+import com.reservation.ensi.DTO.PlaneDTO;
+import com.reservation.ensi.model.Plane;
+import com.reservation.ensi.persistence.PlaneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlaneService {
+
+    @Autowired
+    private PlaneRepository planeRepository;
+
+    public List<Plane> findAll(){
+        return this.planeRepository.findAll();
+    }
+
+    public Plane create(PlaneDTO planeDTO){
+        return this.planeRepository.save(
+                new Plane( planeDTO.getType(),planeDTO.isAvailable(), planeDTO.getNbPlace()));
+    }
+
+
+    public void save(Plane plane) {
+        planeRepository.save(plane);
+    }
+
+    public void deleteById(Long id){
+        this.planeRepository.deleteById(id);
+    }
+
+}
+
+
