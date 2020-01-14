82fe211f7f29e78cf8be6b821eddaca54effad98
Iheb Ben Ayed <iheb.benayed@ensi-uma.tn>
2020-01-14 20:10:59 +0100

    final

--- ReservationMicroservices/user/src/main/java/com/ensi/user/controller/UserController.java
+++ ReservationMicroservices/user/src/main/java/com/ensi/user/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 //@CrossOrigin(origins = {"http://localhost:8082","http://localhost:8081"})
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserController {
 
 	@Autowired
